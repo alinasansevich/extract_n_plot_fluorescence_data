06ac7e191f21a332cd81d7b557f6e4977ba73be2
Alina Sansevich <alinasansevich@gmail.com>
2020-09-24 16:27:06 -0500

    Extract and plot all data.

--- extract_n_plot_fluo_data.py
+++ extract_n_plot_fluo_data.py
@@ -17,35 +17,16 @@ import os
 import csv
 import itertools
 
-import datetime
+# import datetime
 import pygal
+import matplotlib.pyplot as plt
 
-#######
-# bosquejo inicial:
-
-# data = {}
-
-# # using os module
-# for file in directory:
-#     reader = csv.reader()
-#     # read until reader == ROX -- or go to line xxx
-#     data[primeros8chardelfile] = [reader]
-#     # y despues .append las demas lineas
-
-# plot
-
-# usar datetime para transformar cada key en un datetime object
-
-# box-plot
-
-# filename_1 = "20200102113551_00890018002017-00320181107-874282.csv"
-
-######
 # program starts here
 file_path = '/home/alina/Learning_to_Code/My_Projects/raw_readings/test_files'
 
 # these are the files to iterate over:
 test_files = os.listdir(file_path)
+test_files.sort()
 
 all_data = {}
 
@@ -53,18 +34,21 @@ start = 46
 stop = 62
 
 for filename in test_files:
-    raw_data =[]
-    for i in range(start, stop):
-        with open(filename) as f:
-            row = next(itertools.islice(csv.reader(f), i, None))
-            raw_data.append(row)
+    try:
+        raw_data =[]
+        for i in range(start, stop):
+            with open(filename) as f:
+                row = next(itertools.islice(csv.reader(f), i, None))
+                raw_data.append(row)
+    except StopIteration:
+        continue
     
     file_date = filename[0:8]
     data_points_lists = []
     for item in raw_data:
         x = item[1:len(item)-1]   # I only need raw_data[item][1:25]
         data_points_lists.append(x)
-    data_points = [data_point for inner_list in data_points_lists for data_point in inner_list]
+    data_points = [float(data_point) for inner_list in data_points_lists for data_point in inner_list]
 
     if file_date not in all_data.keys():
         all_data[file_date] = data_points
@@ -73,4 +57,22 @@ for filename in test_files:
             all_data[file_date].append(data_point)
 
 
+# Plot with matplotlib.
+data = [value for value in all_data.values()]
+dates = [key for key in all_data.keys()]
 
+fig1, ax1 = plt.subplots()
+ax1.set_title("Fluorescence vs. date")
+ax1.boxplot(data)
+ax1.set_xlabel("Days")
+ax1.set_ylabel("Fluorescence")
+ax1.set_xticklabels(dates, rotation=30, fontsize=10)
+plt.show()
+
+
+# Plot with pygal.
+box_plot = pygal.Box()
+box_plot.title = "Fluorescence vs. date"
+for key, value in all_data.items():
+    box_plot.add(key, value)
+box_plot.render_to_file('fluorescence_vs_date.svg')
